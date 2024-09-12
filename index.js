018fe3b56c69141e7f340ab0bce7755a44f78248
Moshe Goldstein <mygguy613@gmail.com>
2024-09-12 17:42:15 -0400

    cl

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(names) {
+    return names.slice(0, 2)
+};
+
+// console.log(returnFirstTwoDrivers(drivers));
+
+const returnLastTwoDrivers = function(names) {
+    return names.slice(names.length - 2, names.length)
+}
+
+// console.log(returnLastTwoDrivers(drivers));
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// console.log(selectingDrivers[0](drivers));
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+// console.log(fareDoubler(16));
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(names, selection) {
+    if (selection === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(names);
+    } else if (selection === returnLastTwoDrivers) {
+        return returnLastTwoDrivers(names);
+    }
+}
+
+// console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers));
